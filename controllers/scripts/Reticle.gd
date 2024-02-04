8034926d81997d361272dc6b29bb8765cb129788
NilTPL <76227208+NilTPL@users.noreply.github.com>
2024-02-04 20:17:30 +0100

    EO4 done
    made crosshair dynamic

--- controllers/scripts/Reticle.gd
+++ controllers/scripts/Reticle.gd
@@ -13,7 +13,18 @@ func _ready():
 	queue_redraw()
 
 func _process(delta):
-	pass
+	adjust_reticle_lines()
 
 func _draw():
 	draw_circle(Vector2(0,0), DOT_RADIUS, DOT_COLOR)
+
+func adjust_reticle_lines():
+	var vel = PLAYER_CONTROLLER.get_real_velocity()
+	var origin = Vector3(0,0,0)
+	var pos = Vector2(0,0)
+	var speed = origin.distance_to(vel)
+
+	RETICLE_LINES[0].position = lerp(RETICLE_LINES[0].position, pos + Vector2(0, -speed * RETICLE_DISTANCE), RETICLE_SPEED)
+	RETICLE_LINES[1].position = lerp(RETICLE_LINES[1].position, pos + Vector2(speed * RETICLE_DISTANCE, 0), RETICLE_SPEED)
+	RETICLE_LINES[2].position = lerp(RETICLE_LINES[2].position, pos + Vector2(0, speed * RETICLE_DISTANCE), RETICLE_SPEED)
+	RETICLE_LINES[3].position = lerp(RETICLE_LINES[3].position, pos + Vector2(-speed * RETICLE_DISTANCE, 0), RETICLE_SPEED)
