dfbe2809b6a6ae692305eec8708fdc6be1de421e
NilTPL <76227208+NilTPL@users.noreply.github.com>
2024-03-20 14:53:13 +0100

    FOV tween woohoo

--- controllers/scripts/SprintingPlayerState.gd
+++ controllers/scripts/SprintingPlayerState.gd
@@ -5,13 +5,25 @@ class_name SprintingPlayerState extends PlayerMovementState
 @export var DECELERATION: float = 0.25
 @export var TOP_ANIM_SPEED: float = 1.6
 
+var tween: Tween = null
+
 func enter() -> void:
-	#ANIMATIONPLAYER.play("SprintFOV", -1.0, 1.0)
+	
 	ANIMATIONPLAYER.play("Sprint", 0.5, 1.0)
 	
+	if tween:
+		tween.kill()
+	tween = get_tree().create_tween()
+	tween.tween_property(PLAYER.CAMERA_CONTROLLER, "fov", 80.0, 0.1)
 
 func exit() -> void:
 	ANIMATIONPLAYER.speed_scale = 1.0
+	
+	if tween:
+		tween.kill()
+	tween = get_tree().create_tween()
+	tween.tween_property(PLAYER.CAMERA_CONTROLLER, "fov", 75.0, 0.1)
+	
 func update(delta):
 	PLAYER.update_gravity(delta)
 	PLAYER.update_input(_SPEED, ACCELERATION, DECELERATION)
@@ -26,3 +38,4 @@ func update(delta):
 func set_anim_speed(spd) -> void:
 	var alpha = remap(spd, 0.0, _SPEED, 0.0, 1.0)
 	ANIMATIONPLAYER.speed_scale = lerp(0.0, TOP_ANIM_SPEED, alpha)
+
